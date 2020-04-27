df1aeca18b756299a32b59d28eebb7c4026288fc
Tanya Leshchenko <t.m.leshchenko@gmail.com>
2020-04-27 18:50:01 +0300

    code refactor

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,14 +1,16 @@
 module Api
   module V1
     class ArticlesController < ApplicationController
+
+      before_action :set_article, only: [:show, :update, :destroy]
+
       def index
         articles = Article.order('created_at DESC')
         render json: {status: 'SUCCESS', message: 'Loaded articles', data: articles}, status: :ok
       end
 
       def show
-        article = Article.find(params[:id])
-        render json: {status: 'SUCCESS', message: 'Loaded articles', data: article}, status: :ok
+        render json: {status: 'SUCCESS', message: 'Loaded articles', data: @article}, status: :ok
       end
 
       def create
@@ -21,21 +23,23 @@ module Api
       end
 
       def update
-        article = Article.find(params[:id])
-        if article.update_attributes(article_params)
-          render json: {status: 'SUCCESS', message: 'Updated article', data: article}, status: :ok
+        if @article.update_attributes(article_params)
+          render json: {status: 'SUCCESS', message: 'Updated article', data: @article}, status: :ok
         else
-          render json: {status: 'ERROR', message: 'Article not updated', data: article.errors}, status: :unprocessable_entity
+          render json: {status: 'ERROR', message: 'Article not updated', data: @article.errors}, status: :unprocessable_entity
         end
       end
 
       def destroy
-        article = Article.find(params[:id])
-        article.destroy
-        render json: {status: 'SUCCESS', message: 'Deleted article', data: article}, status: :ok
+        @article.destroy
+        render json: {status: 'SUCCESS', message: 'Deleted article', data: @article}, status: :ok
       end
 
       private
+
+      def set_article
+        @article = Article.find(params[:id])
+      end
 
       def article_params
         params.permit(:title, :body)
